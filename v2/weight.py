3b98f96454d833d3f70ae85c0c306aa8a0f3f10e
epiglottis <epiglottis.cartilage@gmail.com>
2025-12-20 23:48:07 +0800

    weight

---
+++ v2/weight.py
@@ -0,0 +1,26 @@
+import time
+from hx711 import HX711
+
+
+def init():
+    global dev
+    dev = HX711(5, 6)
+    dev.setReferenceUnit(100_000)
+
+
+def read():
+    raw = dev.getRawBytes()
+    weight = dev.rawBytesToWeight(raw)
+    return weight
+
+
+def main():
+    cam = 0  # int(sys.argv[1]) if len(sys.argv) > 1 else 0
+    init(cam)
+    while True:
+        time.sleep(0.1)
+        print(read())
+
+
+if __name__ == "__main__":
+    main()
