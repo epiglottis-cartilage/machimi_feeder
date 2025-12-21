86a84c1cd210d5dc1517df2bd889253359a21205
epiglottis <epiglottis.cartilage@gmail.com>
2025-12-21 16:56:15 +0800

    non-block

--- v2/motor.py
+++ v2/motor.py
@@ -1,18 +1,38 @@
 import uln2003
+from queue import Queue
+import threading
+
+killed = False
+
+
+def _worker():
+    global cmds, dev
+    while not killed:
+        step = cmds.get(timeout=1)
+        if step is None:
+            continue
+        dev.rotate(step)
 
 
 def init():
-    global dev
-    dev = uln2003.ULN2003([17, 18, 27, 22], step=3)
+    global dev, cmds, t
+    cmds = Queue(maxsize=16)
+    dev = uln2003.ULN2003([17, 18, 27, 22], step=2)
+    t = threading.Thread(target=_worker, daemon=True)
+    t.start()
 
 
 def rotate(steps):
-    dev.rotate(steps)
+    global cmds
+    cmds.put(int(steps))
 
 
 def close():
-    global dev
+    global dev, killed, cmds, t
     del dev
+    killed = True
+    t.join()
+    del cmds
 
 
 def main():
@@ -22,6 +42,7 @@ def main():
     print("-")
     rotate(-300)
     print(".")
+    close()
 
 
 if __name__ == "__main__":
